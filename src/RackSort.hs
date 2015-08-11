0ddbd1d865ced4447c86bfc8fc58560e1752dc7f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-08-11 15:50:07 +0100

    A bit more work done

--- src/RackSort.hs
+++ src/RackSort.hs
@@ -1,10 +1,31 @@
-import Data.List (intersperse)
+import Data.List (intersperse,partition)
 
-solved :: String -> Bool
-solved = (=="RYRRBYYRYRRYYRY")
+type Rack = String
 
-valid :: String -> Bool
-valid r =
+data Move
+    = Swap Int Int Rack Rack
+    -- | Cw String Rack
+    -- | Ccw String Rack
+
+type Solution = [Move]
+
+correctRack :: Rack
+correctRack = "RYRRBYYRYRRYYRY"
+
+yellowIndices :: [Int]
+yellowIndices = [1,5,6,8,11,12,14]
+
+redIndices :: [Int]
+redIndices = [0,2,3,7,9,10,13]
+
+blackIndices :: [Int]
+blackIndices = [4]
+
+finishedRack :: Rack -> Bool
+finishedRack = (==correctRack)
+
+validRack :: Rack -> Bool
+validRack r =
     length r == 15 &&
     numColour 'Y' 7 &&
     numColour 'R' 7 &&
@@ -13,7 +34,23 @@ valid r =
         numColour :: Char -> Int -> Bool
         numColour c n = length (filter (==c) r) == n
 
-draw :: String -> IO ()
+wrongIndices :: Rack -> [Int]
+wrongIndices r =
+    map (\(idx, _, _) -> idx) $ filter (\(_, a, b) -> a /= b) xs
+    where
+        xs = zip3 [0..] r correctRack
+
+findSolutions :: Rack -> [Solution]
+findSolutions r = []
+-- find wrong indices for each colour in r
+-- for each wrong index:
+-- build a map of wrong index to list of available correct indices
+-- e.g. given a Y in wrong place, build a list of all other yellow
+-- indices currently occupied by non-Y.
+-- then what ?
+-- iterate over the map making swaps
+
+draw :: Rack -> IO ()
 draw r =
     mapM_ putStrLn $ map renderLine [0..4]
     where
@@ -26,5 +63,6 @@ draw r =
 main :: IO ()
 main = do
     let r = "RYRRBYYRYRRYYRY"
-    putStrLn $ "valid r = " ++ show (valid r)
+    putStrLn $ "finishedRack r = " ++ show (finishedRack r)
+    putStrLn $ "validRack r = " ++ show (validRack r)
     draw r
